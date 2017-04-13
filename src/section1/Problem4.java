f2c188f92e44f75fab429de174d07b43bbc2ba82
ibo <ibobafumba@gmail.com>
2017-04-13 14:42:19 -0400

    Added code to show running time of solution

--- src/section1/Problem4.java
+++ src/section1/Problem4.java
@@ -1,6 +1,9 @@
 package section1;
 
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Problem4 {
     public static boolean isPalindromic(int n){
         String num = Integer.toString(n);
@@ -15,6 +18,7 @@ public class Problem4 {
     }
 
     public static void main(String[] args) {
+        Instant start = Instant.now();
         int largest = 0;
         for (int i = 100; i < 1000 ; i++) {
             for (int j = 100; j < 1000; j++) {
@@ -27,5 +31,9 @@ public class Problem4 {
         }
 
         System.out.println("largest palindromic number is : "+largest);
+        Instant end = Instant.now();
+
+        Duration elapsed = Duration.between(start,end);
+        System.out.println("elapsed : "+elapsed.toMillis()+" millisecond(s)");
     }
 }
